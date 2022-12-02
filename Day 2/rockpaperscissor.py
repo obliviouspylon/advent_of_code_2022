b49565310362fa4136941d8d453137efc476f9ba
James Luo <contact@jamesckluo.ca>
2022-12-02 00:49:43 -0500

    Use hash dictionary

--- Day 2/rockpaperscissor.py
+++ Day 2/rockpaperscissor.py
@@ -1,5 +1,21 @@
 
-
+choice1 = {
+    "A" : {
+        "1": 3,
+        "2" : 6,
+        "3" : 0
+    },
+    "B" : {
+        "1": 0,
+        "2" : 3,
+        "3" : 6
+    },
+    "C" : {
+        "1": 6,
+        "2" : 0,
+        "3" : 3
+    },
+}
 
 def part1(fileName):
     score = 0
@@ -7,9 +23,9 @@ def part1(fileName):
         line = f.readline()
         while line:
             # Let 1 be Rock, 2 be paper, 3 for scissor
-            action = line.replace("\n","").replace("A","1").replace("X","1").replace("B","2").replace("Y","2").replace("C","3").replace("Z","3")
+            action = line.replace("\n","").replace("X","1").replace("Y","2").replace("Z","3")
             action = action.split(" ")
-            score = score + int(action[1])
+            score = score + int(action[1]) + choice1[action[0]][action[1]]
 
             if action[1] == action[0]:
                 score = score + 3
@@ -22,7 +38,7 @@ def part1(fileName):
             line = f.readline()
     return score
 
-choice = {
+choice2 = {
     "A" : {
         "0": 3,
         "3" : 1,
@@ -47,12 +63,12 @@ def part2(fileName):
             # Let 1 be Rock, 2 be paper, 3 for scissor
             action = line.replace("\n","").replace("X","0").replace("Y","3").replace("Z","6")
             action = action.split(" ")
-            score = score + int(action[1]) + choice[action[0]][action[1]]
+            score = score + int(action[1]) + choice2[action[0]][action[1]]
             line = f.readline()
     return score
 
 if __name__ == "__main__":
-    # print(part1("Day 2\\input.txt"))
-    print(part2("Day 2\\input.txt"))
-    # print(part1("Day 2\\example.txt"))
-    print(part2("Day 2\\example.txt"))
+    print(part1("Day 2\\input.txt"))
+    # print(part2("Day 2\\input.txt"))
+    print(part1("Day 2\\example.txt"))
+    # print(part2("Day 2\\example.txt"))
