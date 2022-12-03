0c5112ae8408a97a699d1539e45fcb76916470c7
James Luo <contact@jamesckluo.ca>
2022-12-03 00:27:31 -0500

    Added template

---
+++ template.py
@@ -0,0 +1,22 @@
+
+
+def part1(fileName):
+    score = 0
+    with open(fileName,"r") as f:
+        line = f.readline()
+        while line:
+            # Code goes here
+
+            
+
+            line = f.readline()
+    return score
+
+
+
+
+if __name__ == "__main__":
+    print(part1("Day 3\\input.txt"))
+    print(part1("Day 3\\example.txt"))
+    # print(part2("Day 3\\input.txt"))
+    # print(part2("Day 3\\example.txt"))
